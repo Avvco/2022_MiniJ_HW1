8302a9dc08e2abed8ac29629ffead6a2894a0291
Avvco <leei910110@gmail.com>
2022-03-26 19:22:00 +0800

    update build.bat

--- build.bat
+++ build.bat
@@ -1,10 +1,8 @@
-set mypath=%cd%
-@echo %mypath%
-docker run -it --rm -v %mypath%:/tmp -w /tmp knordman/bison-flex-builder make clean
-docker run -it --rm -v %mypath%:/tmp -w /tmp knordman/bison-flex-builder make debug
-docker run -it --rm -v %mypath%:/tmp -w /tmp knordman/bison-flex-builder flex -o minij_lex.c minij_lex.l
-docker run -it --rm -v %mypath%:/tmp -w /tmp knordman/bison-flex-builder gcc -c minij_lex.c
-docker run -it --rm -v %mypath%:/tmp -w /tmp knordman/bison-flex-builder gcc -c minij_parse.c
-docker run -it --rm -v %mypath%:/tmp -w /tmp knordman/bison-flex-builder gcc -c minij.c
-docker run -it --rm -v %mypath%:/tmp -w /tmp knordman/bison-flex-builder gcc -o mjparse.exe minij.o minij_lex.o minij_parse.o
+docker run -it --rm -v %cd%:/tmp -w /tmp knordman/bison-flex-builder make clean
+docker run -it --rm -v %cd%:/tmp -w /tmp knordman/bison-flex-builder make debug
+docker run -it --rm -v %cd%:/tmp -w /tmp knordman/bison-flex-builder flex -o minij_lex.c minij_lex.l
+docker run -it --rm -v %cd%:/tmp -w /tmp knordman/bison-flex-builder gcc -c minij_lex.c
+docker run -it --rm -v %cd%:/tmp -w /tmp knordman/bison-flex-builder gcc -c minij_parse.c
+docker run -it --rm -v %cd%:/tmp -w /tmp knordman/bison-flex-builder gcc -c minij.c
+docker run -it --rm -v %cd%:/tmp -w /tmp knordman/bison-flex-builder gcc -o mjparse.exe minij.o minij_lex.o minij_parse.o
 PAUSE
